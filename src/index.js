0c38506a868d7c82a887a9d9800bc7fea6737a80
Sharimkhan01 <sharimkhan22@gmail.com>
2025-02-03 23:23:14 +0530

    added env variables

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const logger  = require('./middleware/logger');
 const router = require('./users/router');
+const dotenv = require('dotenv')
+dotenv.config();
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+
+console.log(process.env.HOST);
+
 
 app.use(express.json());
 app.use(logger);
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv')
+dotenv.config();
+// console.log(process.env.USER);
+
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'SqlKhan@123',
-  database: 'l1_training'
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 })
 
 db.connect((err) => {
